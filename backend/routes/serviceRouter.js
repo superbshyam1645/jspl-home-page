990dd017cc571fe82ea61f4a4a9cecd28c032978
Shyam Jee <jees8269@gmail.com>
2024-11-04 16:54:46 +0000

    Changing 115\6

--- backend/routes/serviceRouter.js
+++ backend/routes/serviceRouter.js
@@ -1,4 +1,13 @@
-const { Router } = require("react");
+const { Router } = require("express");
+const {
+  getTechnologies,
+  getStaffing,
+  getHospitality,
+} = require("../controller/serviceController");
 const app = Router();
 
-app.get("/s");
+app.get("/staffing", getStaffing);
+app.get("/technology", getTechnologies);
+app.get("/hospitality", getHospitality);
+
+module.exports = app;
